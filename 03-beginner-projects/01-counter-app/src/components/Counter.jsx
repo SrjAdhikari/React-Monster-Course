573d25339b15d263b9cf1f6975ebb1aaaa84e1ff
SrjAdhikari <surajadhikari055@gmail.com>
2025-06-25 12:02:40 +0900

    feat: add 01-counter-app project with main app setup

---
+++ 03-beginner-projects/01-counter-app/src/components/Counter.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "../style.css";
+
+const Counter = () => {
+	const [count, setCount] = useState(0);
+
+	const handleIncrement = () => setCount(count + 1);
+	const handleDecrement = () => setCount(count - 1);
+
+	return (
+		<>
+			<div className="container">
+				<h1 className="number">Count: {count}</h1>
+			</div>
+
+			<section className="btns-container">
+				<button onClick={handleIncrement} className="increment">
+					+
+				</button>
+
+				<button onClick={handleDecrement} className="decrement">
+					-
+				</button>
+			</section>
+		</>
+	);
+};
+
+export default Counter;
